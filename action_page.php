a123bc3048f58f2a2df1b48eadc684dc1149d378
JET-6 <thomas.jimi@gmail.com>
2021-05-06 10:34:11 -0700

    Initial commit

---
+++ action_page.php
@@ -0,0 +1,7 @@
+
+DO ALL FORM SUBMISSIONS REQUIRE JS TO READ?
+
+
+if you're reading this dan, this is something i hope to implement in the future & maybe make functional in a push to github.
+
+THEREBY COLLECTING SUGGESTIONS FROM PEOPLE ABOUT COOL LINKS!
